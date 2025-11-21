4c917aedd6db793f0344b8f5d299c4fb63fc87af
Prasanna H <prasannahanchinamani@gmail.com>
2025-11-21 19:53:45 +0530

    edit contacts based on name

--- AddressbookSystem/AddressBook.cs
+++ AddressbookSystem/AddressBook.cs
@@ -12,18 +12,46 @@ namespace AddressBookSystem
             contacts = new List<Contacts>();
         }
 
-
+        // Add a new contact
         public void AddContact(Contacts contact)
         {
             contacts.Add(contact);
             Console.WriteLine("Contact added successfully!");
         }
-        public void DisplayContacts()
+
+        // Edit an existing contact by first name
+        public void EditContact(string firstName)
         {
-            Console.WriteLine("Dispalaying contacts");
-            foreach (var contact in contacts)
+            firstName = firstName.ToLower();
+            Contacts contactToEdit = contacts.Find(c => c.FirstName.ToLower().Equals(firstName));
+
+            if (contactToEdit != null)
             {
-                Console.WriteLine(contact);
+                Console.WriteLine($"Editing contact: {contactToEdit.FirstName} {contactToEdit.LastName}");
+
+                Console.Write("Enter new address: ");
+                contactToEdit.Address = Console.ReadLine();
+
+                Console.Write("Enter new city: ");
+                contactToEdit.City = Console.ReadLine();
+
+                Console.Write("Enter new state: ");
+                contactToEdit.State = Console.ReadLine();
+
+                Console.Write("Enter new zip: ");
+                contactToEdit.Zip = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter new phone number: ");
+                contactToEdit.PhoneNumber = long.Parse(Console.ReadLine());
+
+                Console.Write("Enter new email: ");
+                contactToEdit.Email = Console.ReadLine();
+
+                Console.WriteLine("Contact updated successfully!");
+            }
+            else
+            {
+                Console.WriteLine($"Contact with name '{firstName}' not found.");
             }
         }
     }
